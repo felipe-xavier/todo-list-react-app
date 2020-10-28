53de8ee4f682686e83b4400a55c115fb45db1751
felipe.xavier <felipe.xavier@funceme.br>
2020-10-28 19:07:25 -0300

    Add About component and Route.

---
+++ src/components/about/About.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1> About </h1>
+        <p>A Todo App</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default About;
